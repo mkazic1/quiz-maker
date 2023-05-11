6c0b61b41f51adac20d5842ae1e1b0a1b5371cdd
Medina <mkazic1@etf.unsa.ba>
2023-05-11 16:54:33 +0200

    Welcome page done

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,16 +1,24 @@
-import { CardMedia } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Button, CardMedia, Box } from '@mui/material';
+import ROUTES from '../constants/routes';
 import quizPic from '../assets/quiz.png';
+import { linkStyle, pageLayoutStyle } from '../style/CommonStyle';
 
 const WelcomePage = () => (
-  <CardMedia
-    image={quizPic}
-    alt="Quiz intro"
-    component="img"
-    sx={{
-      width: '100%',
-      height: '100vh',
-    }}
-  />
+  <Box sx={pageLayoutStyle}>
+    <CardMedia
+      image={quizPic}
+      alt="Quiz intro"
+      component="img"
+      sx={{
+        width: '30%',
+        height: '30%',
+      }}
+    />
+    <Link to={`${ROUTES.QUIZZES}`} style={linkStyle}>
+      <Button variant="contained">START</Button>
+    </Link>
+  </Box>
 );
 
 export default WelcomePage;
