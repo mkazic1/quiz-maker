b426e6ec1d7e7ad045bc677f23c4548152fc7ded
Medina <mkazic1@etf.unsa.ba>
2023-05-11 22:22:04 +0200

    Table view and additional files implemented

---
+++ src/constants/table.js
@@ -0,0 +1,20 @@
+const TABLE_HEADER = [
+  {
+    title: 'Quiz name',
+    key: 'name',
+  },
+  {
+    title: 'Try out',
+    key: 'try-out',
+  },
+  {
+    title: 'Edit',
+    key: 'edit',
+  },
+  {
+    title: 'Delete',
+    key: 'delete',
+  },
+];
+
+export default TABLE_HEADER;
