6f0f75dfddbbedcbcf1c823d368f7820662e2ee3
Medina <mkazic1@etf.unsa.ba>
2023-05-11 22:21:26 +0200

    Styling added

---
+++ src/style/ModalStyle.js
@@ -0,0 +1,26 @@
+export const dialogTitleBox = {
+  textAlign: 'center',
+};
+
+export const titleTypographyStyle = {
+  fontWeight: 'bold',
+  color: 'secondary.dark',
+  display: 'block',
+};
+
+export const iconStyle = {
+  color: 'secondary.dark',
+  fontSize: 60,
+};
+
+export const dialogActionStyle = {
+  justifyContent: 'space-between',
+  display: 'flex',
+  pb: 3,
+  px: 3,
+};
+
+export const textfieldStyle = {
+  width: '100%',
+  paddingY: '10px',
+};
---
+++ src/style/TableStyle.js
@@ -0,0 +1,25 @@
+export const headerCellStyle = {
+  border: '1px solid lightgray',
+  textAlign: 'center',
+  fontSize: 18,
+  letterSpacing: '0.1rem',
+  backgroundColor: 'primary.main',
+  color: 'text.secondary',
+  padding: '10px',
+  width: '20%',
+};
+
+export const rowCellStyle = {
+  borderLeft: '1px solid lightgray',
+  textAlign: 'center',
+  letterSpacing: '0.1rem',
+  padding: '10px',
+  width: '20%',
+};
+
+export const tableBox = {
+  padding: '10px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
